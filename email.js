c31606590f4e2449a84f897e7a1f343a38e20db6
Saivyshnavi-kolla <156276128+Saivyshnavi-kolla@users.noreply.github.com>
2024-02-18 22:54:34 -0500

    Update email.js

--- email.js
+++ email.js
@@ -4,7 +4,7 @@ function showhideEmail(){
 		document.getElementById('email').innerHTML = "Show my email";
 		shown = false;
 	}else {
-		var myemail ="<a href='mailto:kollasv"+ "@" + "mail.uc.edu'>kollasv" + "@" + "mail.uc,edu</a>";
+		var myemail ="<a href='mailto:kollasv"+ "@" + "mail.uc.edu'>kollasv" + "@" + "mail.uc.edu</a>";
 		document.getElementById('email').innerHTML=myemail;
 		shown=true;
 	}
